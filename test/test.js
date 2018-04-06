5d21b1bac7dc9247b24082bcfa9413deed1da514
Talon <pvpgamecrash@gmail.com>
2018-04-05 18:51:57 -0700

    more tests

--- test/test.js
+++ test/test.js
@@ -4,8 +4,28 @@ const expect = require('chai').expect;
 const palette = require('../palette.js');
 
 describe('#palette', function() {
-    it('should not equal "string"', function() {
+    it('should be typed as undefined"', function() {
+        var result = palette('red', 'message');
+
+        expect(result).to.be.an('undefined');
+      });
+      it('should be typed as undefined"', function() {
+        var result = palette('yellow', 'message');
+
+        expect(result).to.be.an('undefined');
+      });
+      it('should be typed as undefined"', function() {
         var result = palette('green', 'message');
+
+        expect(result).to.be.an('undefined');
+      });
+      it('should be typed as undefined"', function() {
+        var result = palette('blue', 'message');
+
+        expect(result).to.be.an('undefined');
+      });
+      it('should be typed as undefined"', function() {
+        var result = palette('purple', 'message');
 
         expect(result).to.be.an('undefined');
       });
