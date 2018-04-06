ea8b649c37fb85b5ba0fb1b7cf5a7f91f5e9e9b6
Jiří Helebrant <jiri.helebrant@nic.cz>
2018-04-06 14:40:53 +0200

    Use a color map instead of bunch of IFs - use a single IF to check whether the color is supported - print message without formatting if an unsupported color is requested - add cyan
    Fixes #2.

--- palette.js
+++ palette.js
@@ -1,22 +1,16 @@
+const colors = {
+  red: 31,
+  green: 32,
+  yellow: 33,
+  blue: 34,
+  purple: 35,
+  cyan: 36,
+  white: 37
+};
+
 module.exports = function palette(color, message) {
-
-  if(color === "red") {
-    return console.log('\x1b[31m%s\x1b[0m', message);
+  if (colors[color]) {
+    return console.log(`\x1b[${colors[color]}m%s\x1b[0m`, message);
   }
-  if(color === "yellow") {
-    return console.log('\x1b[33m%s\x1b[0m', message);
-  }
-  if(color === "green") {
-    return console.log('\x1b[32m%s\x1b[0m', message);
-  }
-  if(color === "blue") {
-    return console.log('\x1b[34m%s\x1b[0m', message);
-  }
-  if(color === "purple") {
-    return console.log('\x1b[35m%s\x1b[0m', message);
-  }
-  if (color === "white") {
-    return console.log('\x1b[37m%s\x1b[0m', message);
-  }
-
+  return console.log(message);
 };
